aaa536441df45e8372ab078100d3b4722e682b3e
Cage <gousaid67@gmail.com>
2021-11-17 20:48:37 +0100

    First

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate json;
+extern crate rend3;
+use std::path;
+
+fn main() {
+    let currpath = std::env::current_dir().unwrap();
+    let currpath_str = currpath.as_os_str();
+    let file = s
+}
