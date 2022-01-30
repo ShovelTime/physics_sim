38a12c42b59453aa78f43e3a7c033f2d1c3a84bd
Cage <gousaid67@gmail.com>
2022-01-30 16:31:58 +0100

    lin_alg fnc

--- src/math/lin_alg.rs
+++ src/math/lin_alg.rs
@@ -2,5 +2,7 @@ use crate::math::vec::Vec3;
 
 pub fn draw_sphere(radius : f32, position : Vec3, sectorcount : i32, stackcount : i32 ) -> Vec<f32>
 {
-    
-}
+    let (x, y, z , xy) : (f32, f32, f32, f32);
+    let (sectorangle, stackangle) : (f32, f32);
+    let (s, t) : (f32, f32);
+} 
